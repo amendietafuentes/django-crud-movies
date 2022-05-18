cdd4cc6d51f98e889faa90979b8a3e4f929a3092
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-05-18 02:53:30 -0500

    Added connection Django  to MySQL database

--- crudmovies/settings.py
+++ crudmovies/settings.py
@@ -78,8 +78,16 @@ WSGI_APPLICATION = 'crudmovies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'crud_movies05172022',
+        'USER':  'root',
+        'PASSWORD':  '',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'CONN_MAX_AGE': 9,  #  than wait _timeout smaller 
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1;",
+        }
     }
 }
 
