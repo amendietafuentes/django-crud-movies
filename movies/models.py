a865115c3e864c3be3951df6faddf470644783e3
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-05-18 04:56:47 -0500

    List movie information on table html

--- movies/models.py
+++ movies/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 # Create your models here.
 
-class Movies(models.Model):
+class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     cover = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Cover')
     title = models.CharField(max_length=100, verbose_name='Title')
     description = models.TextField(null=True, blank=True, max_length=500, verbose_name='Description')
 
+def __str__(self):
+    row = "Title: " + self.title + " - " + "Description: " + self.description
+    return row 
 
+def delete(self, using=None, keep_parents=False):
+    self.cover.storage.delete(self.cover.name)
+    super().delete()
--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,17 @@
 from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
+
 def movies(request):
-    return render(request, 'movies/index.html')
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
 def add(request):
     return render(request, 'movies/create.html')
+
 def edit(request):
     return render(request, 'movies/edit.html')
