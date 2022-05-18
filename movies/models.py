9950b6a2fb8bc8753354b19f527172f552e3b6e1
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-05-18 04:11:33 -0500

    Register model movie to admin django to insert initials movies content to system

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Movies(models.Model):
+    id = models.AutoField(primary_key=True)
+    cover = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Cover')
+    title = models.CharField(max_length=100, verbose_name='Title')
+    description = models.TextField(null=True, blank=True, max_length=500, verbose_name='Description')
+
+
