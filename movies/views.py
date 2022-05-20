b071b090f7c1a47e3a726476f31e243dd0c0b753
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-05-19 19:27:41 -0500

    Update create form with info models

--- movies/views.py
+++ movies/views.py
@@ -2,16 +2,16 @@ from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from .forms import MovieForm
 
 # Create your views here.
-
-
 def movies(request):
     movies = Movie.objects.all()
-    return render(request, 'movies/index.html', {'movies': movies})
+    return render(request, 'movies/index.html', {'movies' : movies})
 
 def add(request):
-    return render(request, 'movies/create.html')
+    form = MovieForm(request.POST or None)
+    return render(request, 'movies/create.html', {'form' : form})
 
 def edit(request):
     return render(request, 'movies/edit.html')
